c4ab18559cfd26e02228db6de90540d09a6f896e
Vijay Singh <vijay.singh@service.nsw.gov.au>
2023-02-27 22:09:06 +1100

    Java18 features initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # java18-examples
 java18-examples
+
+This project will have an examples of Java18 features. 
---
+++ src/main/java/com.examples/Examples.java
@@ -0,0 +1,9 @@
+package com.examples;
+
+public class Examples {
+
+    public static void main(String args[]){
+        System.out.println("welcome to java example");
+    }
+}
+
